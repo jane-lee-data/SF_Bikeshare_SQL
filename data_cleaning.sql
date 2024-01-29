33928b06bdb8c749ed3da2f435d8f4b95a13cdd0
Jane Lee <106517222+jane-lee-data@users.noreply.github.com>
2024-01-29 01:38:59 -0800

    Update data_cleaning.sql

--- data_cleaning.sql
+++ data_cleaning.sql
@@ -9,19 +9,28 @@
 */
 
 -- station table
-ALTER TABLE station ADD COLUMN installation_date_temp DATE; -- temporary column
-UPDATE station SET installation_date_temp = STR_TO_DATE(installation_date, '%m/%d/%Y') WHERE installation_date IS NOT NULL; -- format conversion
-ALTER TABLE station DROP COLUMN installation_date; -- drop old column
-ALTER TABLE station CHANGE installation_date_temp installation_date DATE; -- change column name
+ALTER TABLE station 
+    ADD COLUMN installation_date_temp DATE; -- temporary column
+UPDATE station 
+    SET installation_date_temp = STR_TO_DATE(installation_date, '%m/%d/%Y') WHERE installation_date IS NOT NULL; -- format conversion
+ALTER TABLE station 
+    DROP COLUMN installation_date; -- drop old column
+ALTER TABLE station 
+    CHANGE installation_date_temp installation_date DATE; -- change column name
 
 -- weather table
-ALTER TABLE weather ADD COLUMN date_temp DATE;
-UPDATE weather SET date_temp = STR_TO_DATE(date, '%m/%d/%Y') WHERE date IS NOT NULL;
-ALTER TABLE weather DROP COLUMN date;
-ALTER TABLE weather CHANGE date_temp date DATE;
+ALTER TABLE weather 
+    ADD COLUMN date_temp DATE;
+UPDATE weather 
+    SET date_temp = STR_TO_DATE(date, '%m/%d/%Y') WHERE date IS NOT NULL;
+ALTER TABLE weather 
+    DROP COLUMN date;
+ALTER TABLE weather 
+    CHANGE date_temp date DATE;
 
 -- trip table
-ALTER TABLE trip ADD COLUMN (start_date_temp TIMESTAMP, end_date_temp TIMESTAMP);
+ALTER TABLE trip 
+    ADD COLUMN (start_date_temp TIMESTAMP, end_date_temp TIMESTAMP);
 UPDATE trip
     SET start_date_temp = STR_TO_DATE(start_date, '%m/%d/%Y %H:%i') WHERE start_date IS NOT NULL; -- timestamp included
 UPDATE trip
